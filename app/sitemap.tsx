2f7a5d1b649550a3a9b23d69c23372205696d4fb
ahmetulutas <ahmet.ulutas@hangikredi.com>
2024-08-26 14:41:17 +0300

    refactor: revert middleware redirect

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -24,3 +24,22 @@ const staticSiteMapItems: MetadataRoute.Sitemap = staticPageUrls.flatMap((item, 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [...staticSiteMapItems];
 }
+
+// const generateAlternateLinks = (url: string) => {
+//   const languages: Record<string, string> = {};
+//   availableLocales
+//     .filter((item) => item !== defaultLanguage)
+//     .forEach((locale) => (languages[locale] = `${baseUrl}/${locale}${url}`));
+//   return languages;
+// };
+
+// const staticSiteMapItems: MetadataRoute.Sitemap = staticPageUrls.map((item, idx) => {
+//   return {
+//     url: `${baseUrl}${item}`,
+//     lastModified: new Date(),
+//     changeFrequency: 'monthly',
+//     alternates: {
+//       languages: generateAlternateLinks(item),
+//     },
+//   };
+// });
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,10 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from 'next/server';
 
-import { defaultLanguage, availableLocales } from "./i18n/settings";
+import { defaultLanguage, availableLocales } from './i18n/settings';
 
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
-
-  // Check if the default locale is in the pathname
-  if (
-    pathname.startsWith(`/${defaultLanguage}/`) ||
-    pathname === `/${defaultLanguage}`
-  ) {
-    // e.g. incoming request is /en/about
-    // The new URL is now /about
-    return NextResponse.redirect(
-      new URL(
-        pathname.replace(
-          `/${defaultLanguage}`,
-          pathname === `/${defaultLanguage}` ? "/" : ""
-        ),
-        request.url
-      )
-    );
-  }
 
   const pathnameIsMissingLocale = availableLocales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
@@ -34,15 +16,13 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /about
     // Tell Next.js it should pretend it's /en/about
-    return NextResponse.rewrite(
-      new URL(`/${defaultLanguage}${pathname}`, request.url)
-    );
+    return NextResponse.rewrite(new URL(`/${defaultLanguage}${pathname}`, request.url));
   }
 }
 
 export const config = {
   // matcher: '/:lng*'
   matcher: [
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|en/studio|cv.pdf|sitemap.xml).*)", // Captures any string that does not start with one of the specified patterns (api, _next/static, _next/image, assets, favicon.ico, sw.js, en/studio, cv.pdf).
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|en/studio|cv.pdf|sitemap.xml).*)', // Captures any string that does not start with one of the specified patterns (api, _next/static, _next/image, assets, favicon.ico, sw.js, en/studio, cv.pdf).
   ],
 };

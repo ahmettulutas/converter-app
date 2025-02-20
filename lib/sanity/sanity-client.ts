7f193ce8f542575560b9e07ffd91e8a65510360e
ahmettulutas <englishlectures.stocks@gmail.com>
2025-02-20 14:31:26 +0300

    merge: revert dynamic seo

--- lib/sanity/sanity-client.ts
+++ lib/sanity/sanity-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity';
 import { apiVersion, dataset, projectId, useCdn } from './env';
 
-export const SANITY_URL = '/studio';
+export const SANITY_URL = '/en/studio';
 export const client = createClient({
   apiVersion,
   dataset,
